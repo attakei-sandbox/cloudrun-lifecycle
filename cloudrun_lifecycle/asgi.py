c601cb9a1a0ac45e678af47d35f6e1f092986348
Kazuya Takei <myself@attakei.net>
2020-05-14 01:11:46 +0900

    Implement slack

--- cloudrun_lifecycle/asgi.py
+++ cloudrun_lifecycle/asgi.py
@@ -1,16 +1,26 @@
 """ASGI application endpoint module.
 """
+import os
 import logging
 
+import slackweb
 from fastapi import FastAPI
 
 
-logger = logging.getLogger(__name__)
+Logger = logging.getLogger(__name__)
 app = FastAPI()
+slack = slackweb.Slack(url=os.environ["SLACK_WEBHOOK_URL"])
 
 
 @app.on_event("startup")
 async def _startup():
     """ASGI application configuration by environments
     """
-    pass
+    Logger.info("Start container")
+    slack.notify(text="Start container")
+
+
+@app.on_event("shutdown")
+async def _shutdown():
+    slack.notify(text="Shutdown container")
+    Logger.info("Shutdown container")
